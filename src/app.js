598fc904a094467c1007214033fb47453ad3b443
TadeoDaniel19 <hector_lectric@hotmail.com>
2021-07-10 01:04:16 -0500

    serv: add routes, connection to mongodb

---
+++ src/app.js
@@ -0,0 +1,27 @@
+const path = require('path');
+const express = require('express');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+
+const app = express();
+
+// connection to db
+mongoose.connect('mongodb://localhost/marvelDB')
+  .then(db => console.log('db connected'))
+  .catch(err => console.log(err));
+
+// importing routes
+const indexRoutes = require('./routes/index');
+
+// settings
+app.set('port', process.env.PORT || 3000);
+
+// middlewares
+app.use(morgan('dev'));
+app.use(express.urlencoded({extended: false}))
+// routes
+app.use('/', indexRoutes);
+
+app.listen(app.get('port'), () => {
+  console.log(`server on port ${app.get('port')}`);
+});
